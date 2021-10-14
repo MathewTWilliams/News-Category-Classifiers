92bb517e2642dd9d3b3a396e2b594ffbee123de6
Matt Williams <williams.mathew.t@gmail.com>
2021-10-13 21:02:44 -0400

    Submitting constants.py and grab_articles.py.

---
+++ Scripts/constants.py
@@ -0,0 +1,19 @@
+#Author: Matt Williams
+#Version: 10/13/2021
+
+# A Simply Python File that contains constant values that are important
+#to the project. 
+
+DATA_SET_NAME = "..\Data\News_Category_Dataset_v2.json"
+
+ARTICLE_SET_NAME = "..\Data\Article_Set.json"
+
+CATEGORIES = ["POLITICS", "WELLNESS", "ENTERTAINMENT", "TRAVEL", 
+                "STYLE & BEAUTY", "PARENTING", "HEALTHY LIVING", "QUEER VOICES", 
+                "FOOD & DRINK", "BUSINESS", "COMEDY", "SPORTS"]
+
+
+ARTICLES_PER_CATEGORY = 4800
+
+VALD_SET_PERCENT = 10 
+TEST_SET_PERCENT = 10
