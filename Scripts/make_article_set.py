cc337281692a223e81c6964b8d68ad52c123273d
Matt Williams <willia137@students.rowan.edu>
2021-10-21 18:01:39 -0400

    Updating requirements.txt. Changed make_article_set script to use the concurrent.futures library instead of threading

--- Scripts/make_article_set.py
+++ Scripts/make_article_set.py
@@ -1,47 +1,42 @@
 #Author: Matt Williams
-#Version: 10/15/2021
+#Version: 10/21/2021
 
 
 from sort_dataset import sort_dataset
 from web_scraper import check_if_page_exists
 from constants import * 
-import json
 import os
 from save_load_json import load_json,save_json
-import threading
+from concurrent.futures import ThreadPoolExecutor
 import time
 
-#think about storting dataset
-class ArticleSetThread(threading.Thread):
 
-    def __init__(self, category, dataset, delay):
-        threading.Thread.__init__(self)
-        self.article_list = []
-        self.category = category
-        self.dataset = dataset
-        self.delay = delay
 
-    def run(self): 
-        counter = 0
-        while len(self.article_list) < ARTICLES_PER_CATEGORY and counter < len(self.dataset):
-            art_obj = self.dataset[counter]
-            art_cat = art_obj['category'] 
-            art_link = art_obj['link']
-            if art_cat == self.category and check_if_page_exists(art_link): 
-                small_art_obj = {}
-                small_art_obj['link'] = art_link
-                small_art_obj['headline'] = art_obj['headline']
-                small_art_obj['short_description'] = art_obj['short_description']
-                self.article_list.append(small_art_obj)
-            counter += 1
-            print(self.category + ":", len(self.article_list))
-            time.sleep(self.delay)
+def get_articles_for_category(**kwargs):
+    category = kwargs['category']
+    delay = kwargs['delay']
+    dataset = kwargs['dataset']
+    article_list = []
+    counter = 0
 
-    def get_article_list(self):
-        return self.article_list
+    while len(article_list) < ARTICLES_PER_CATEGORY and counter < len(dataset):
+        art_obj = dataset[counter]
+        art_cat = art_obj['category']
+        art_link = art_obj['link']
+        if art_cat == category and check_if_page_exists(art_link): 
+            small_art_obj = {}
+            small_art_obj['link'] = art_link
+            small_art_obj['headline'] = art_obj['headline']
+            small_art_obj['short_description'] = art_obj['short_description']
+            article_list.append(small_art_obj)
+         
+        counter += 1
+        print(category+ ":", len(article_list))
+        time.sleep(delay)
 
-    def get_category(self):
-        return self.category
+    
+    return category, article_list
+
 
 
 
@@ -57,24 +52,24 @@ if __name__ == "__main__":
         sort_dataset()
 
     dataset = load_json(SORTED_DATA_PATH)
-    threads = []
     article_set = {}
     delay = 0.25
-
-    #start our threads
-    for category in CATEGORIES:
-        article_set[category] = []
-        thread = ArticleSetThread(category, dataset[category], delay)
-        threads.append(thread)
-        thread.start()
+    futures = []
 
 
-    #wait for all threads to finish
-    for thread in threads: 
-        thread.join()
+    with ThreadPoolExecutor(max_workers= len(CATEGORIES)) as executor: 
+        #start our threads
+        for category in CATEGORIES:
+            article_set[category] = []
+            article_list = dataset[category]
+            future = executor.submit(get_articles_for_category,
+            category = category, dataset = article_list, delay=delay)
+            futures.append(future)
 
-    for thread in threads: 
-        article_set[thread.get_category()] = thread.get_article_list()
+    
+    for future in futures:
+        category, list = future.result()
+        article_set[category] = list
 
 
     save_json(article_set, ARTICLE_SET_PATH)
