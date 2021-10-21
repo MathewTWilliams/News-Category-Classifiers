3f4f014cf97feb2a56c675ea2dcb3c0b87ac53b4
Matt Williams <williams.mathew.t@gmail.com>
2021-10-21 01:01:45 -0400

    Updated constants.py to use PurePath Library for file path related constants. Changed and/or edited values.

--- Scripts/constants.py
+++ Scripts/constants.py
@@ -1,13 +1,20 @@
 #Author: Matt Williams
-#Version: 10/13/2021
+#Version: 10/19/2021
+
+from pathlib import PurePath, Path
 
 # A Simply Python File that contains constant values that are important
 #to the project. 
 
 #Dataset: https://www.kaggle.com/rmisra/news-category-dataset 
-DATA_SET_PATH = "../Data/News_Category_Dataset_v2.json"
+DATA_SET_PATH = PurePath.joinpath(Path(__file__).resolve().parent.parent,
+                   "Data/News_Category_Dataset_v2.json").as_posix()
 
-ARTICLE_SET_PATH = "../Data/Article_Set.json"
+ARTICLE_SET_PATH = PurePath.joinpath(Path(__file__).resolve().parent.parent,
+                    "Data/Article_Set.json").as_posix()
+
+SORTED_DATA_PATH = PurePath.joinpath(Path(__file__).resolve().parent.parent, 
+                    "Data/Sorted_Data_Set.json").as_posix()
 
 CATEGORIES = ["POLITICS", "WELLNESS", "ENTERTAINMENT", "TRAVEL", 
                 "STYLE & BEAUTY", "PARENTING", "HEALTHY LIVING", "QUEER VOICES", 
@@ -16,5 +23,5 @@ CATEGORIES = ["POLITICS", "WELLNESS", "ENTERTAINMENT", "TRAVEL",
 
 ARTICLES_PER_CATEGORY = 4800
 
-VALD_SET_PERCENT = 10
-TEST_SET_PERCENT = 10
+VALD_SET_SIZE = 480 #10%
+TEST_SET_SIZE = 480 #10%
