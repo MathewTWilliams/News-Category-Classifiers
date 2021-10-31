73a8d8bd5c546a5ba77ae11ab4e0b56876522e7c
Matt Williams <willia137@students.rowan.edu>
2021-10-30 23:37:21 -0400

    submitting work for now to work on different computer. Currently testing text cleaning.

--- Scripts/get_test_text.py
+++ Scripts/get_test_text.py
@@ -1,13 +1,17 @@
 import requests
 
-from text_cleaner import clean_html
+from web_scraper import clean_html
 from constants import TEST_TEXT_PATH
-
+import os 
 
 def get_test_text(): 
     url = "https://www.huffpost.com/entry/texas-amanda-painter-mass-shooting_n_5b081ab4e4b0802d69caad89"
     html = requests.get(url).text
     text = clean_html(html)
+
+    if os.path.exists(TEST_TEXT_PATH): 
+        os.remove(TEST_TEXT_PATH)
+
     with open(TEST_TEXT_PATH, "w+", encoding = "utf-8") as file:
         file.write(text) 
 
